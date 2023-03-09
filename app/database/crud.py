40d271b2de1c8a0bbbeeb1f96f45405368784de8
jhonat23 <jhonat23@gmail.com>
2023-03-08 20:25:26 -0500

    create user added

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,10 +1,13 @@
 # Import models and schemas
-from app.models.models import User
+from app.models.models import User, Country
 from app.schemas import UserSchema
 
 # SQLAlchemy
 from sqlalchemy.orm import Session
 from sqlalchemy import select
+
+# passwords
+import bcrypt
 
 # Read 
 
@@ -22,7 +25,37 @@ def get_user_by_email(
 ):
     pass
 
-def get_users(
-        db: Session,
-):
+def get_users(db: Session):
     pass
+
+def find_country_by_name(
+        db: Session, 
+        name: str
+):
+    return db.query(Country).filter(Country.name == name).first()
+
+# Create
+
+def create_user(
+        db: Session,
+        user: UserSchema
+):
+
+    country = find_country_by_name(
+        db=db,
+        name=user.country
+    ).__dict__
+
+    new_user = User(
+        nickname=user.nickname,
+        password=bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt()),
+        name=user.name,
+        surname=user.surname,
+        email=user.email,
+        phone=user.phone,
+        country_id = country['id']
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
